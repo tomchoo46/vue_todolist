9502ad6fa0c3b7038508ce439300a1bd32e5eefe
Tom Choo <thchoo@snsoft.my>
2018-12-10 11:09:39 +0800

    add to do list to view

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,24 @@
   <div id="app">
     <h1>To-do-list (Vue.js)</h1>
     <to-do-form></to-do-form>
+    <to-do-list :todoList="todoList"></to-do-list>
   </div>
 </template>
 
 <script>
 import ToDoForm from './components/ToDoForm.vue'
+import ToDoList from './components/ToDoList.vue'
 
 export default {
   name: 'app',
+  data(){
+    return{
+      todoList: ['test', 'test2']
+    }
+  },
   components: {
-    "to-do-form": ToDoForm
+    "to-do-form": ToDoForm,
+    "to-do-list": ToDoList
   }
 }
 </script>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ul v-for="(todo, i) in todoList"
+        :key="i">
+      <li>{{todo}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoList",
+  props: ['todoList']
+};
+</script>
