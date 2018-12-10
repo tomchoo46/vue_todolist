3f67aeb6ee21fffaa7538c8eed6c1a6e0600e8fa
Tom Choo <thchoo@snsoft.my>
2018-12-10 10:53:02 +0800

    add todo form in view

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent="requestAdd">
+    <input
+      type="text"
+      id="newTodo"
+      placeholder="New To-do"
+      v-model="formData"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ToDoForm",
+  data(){
+    return {
+        formData: ''
+    } 
+  }
+};
+</script>
